b1dbb5b0922ce05995afa80febe5dd539cfb9ffe
Linh Nguyen <linh.nguyen@primedata.ai>
2023-04-19 15:05:49 +0700

    [source-spotify] destination-track-spotify remove redundant code

--- source-spotify/utils/constants.py
+++
@@ -1,4 +0,0 @@
-SPOTIFY_URL = "https://accounts.spotify.com/"
-SPOTIFY_API_URL = "https://api.spotify.com/"
-SPOTIFY_ACCESS_TOKEN_ENDPOINT = "api/token"
-SPOTIFY_ARTIST_ENDPOINT = "v1/artists/4Z8W4fKeB5YxbusRsdQVPb"
